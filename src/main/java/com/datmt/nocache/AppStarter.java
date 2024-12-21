06cc83435e6e2b5cbf6201be4ab8aa865030e701
datmt <dat.tm24@gmail.com>
2024-12-21 08:52:31 +0700

    fix build

--- src/main/java/com/datmt/nocache/AppStarter.java
+++ src/main/java/com/datmt/nocache/AppStarter.java
@@ -2,6 +2,6 @@ package com.datmt.nocache;
 
 public class AppStarter {
     public static void main(String[] args) {
-        com.datmt.nocache.Main.run();
+        Main.main(args);
     }
 }
--- src/main/java/com/datmt/nocache/Main.java
+++ src/main/java/com/datmt/nocache/Main.java
@@ -5,21 +5,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main extends Application {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
+        logger.info("Starting application");
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
-        primaryStage.setTitle("NO F***ING CACHE BROWSER");
+        primaryStage.setTitle("No Cache Browser");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
+        logger.info("Application window shown");
     }
 
     public static void main(String[] args) {
-        try {
-            launch(args);
-        } catch (Exception e) {
-        }
+        launch(args);
     }
 }
