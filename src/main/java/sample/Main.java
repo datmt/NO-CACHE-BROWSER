cb49c82273b4b3855dff5a376901cff8a751283b
datmt <dat.tm24@gmail.com>
2024-12-21 07:47:16 +0700

    fix language

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
-        primaryStage.setTitle("NO FUCKING CACHE BROWSER");
+        primaryStage.setTitle("NO F***ING CACHE BROWSER");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
